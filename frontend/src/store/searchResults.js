50ddbb3c9b610af7506f78f28cf38c366dd35e49
ramses <ramses.romero.jr@gmail.com>
2021-02-05 13:30:08 -0500

    set up searchResults state

---
+++ frontend/src/store/searchResults.js
@@ -0,0 +1,29 @@
+import {fetch} from './csrf.js'
+
+const SET_SEARCH = 'search/setSearch'
+
+const setSearch = (results) => ({
+    type: SET_SEARCH,
+    results
+});
+
+export const getSearchResults = (search) => async(dispatch)=>{
+    const res = await fetch(`/api/sneaker/search/${search}`)
+
+    dispatch(setSearch(res.data.products));
+    return res;
+}
+
+const initialState = {results: null};
+
+function reducer(state=initialState, action){
+    switch(action.type){
+        case SET_SEARCH:
+            const newState = Object.assign({}, state, {results: action.results});
+            return newState;
+        default:
+            return state
+    }
+}
+
+export default reducer;
