d646bc62dda5b357f0c92dad6e9f87fd7cc7510b
ramses <ramses.romero.jr@gmail.com>
2021-02-08 12:07:29 -0500

    added hover on profile

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -14,8 +14,10 @@ const ProButton = styled.button`
   border:solid 0.5px lightgrey;
   box-shadow: 0 1px 2px 0px rgba(0,0,0,0.6);
   &:hover{
-    background-color:green;
-    color:#FCFAF0;
+    color:green;
+    border:solid 0.5px lightgrey;
+    box-shadow: 0 3px 9px 0px rgba(0,128,0,0.6);
+
   }
 `;
 
