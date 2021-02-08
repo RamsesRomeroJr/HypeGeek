1775cc107f7fb299747b0928a70cf2a6d444d49a
ramses <ramses.romero.jr@gmail.com>
2021-02-08 13:22:55 -0500

    fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -19,8 +19,10 @@ module.exports = {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
     dialectOptions: {
-      ssl:true,
-      rejectUnauthorized:false
+      ssl:{
+        require:true,
+        rejectUnauthorized:false
+      }
     },
     seederStorage: "sequelize",
   },
