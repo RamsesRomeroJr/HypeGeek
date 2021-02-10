614814a0da22906b280d644b5f4ad8a082054bb4
ramses <ramses.romero.jr@gmail.com>
2021-02-10 12:39:43 -0500

    started file

--- frontend/src/components/UserFavorites/index.js
+++ frontend/src/components/UserFavorites/index.js
@@ -1,2 +1,48 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux'
+import {createFavorite, unfavorite, userFav} from '../../store/favorites'
+import { useHistory } from "react-router-dom";
+
+function UserFavorites(){
+    const dispatch = useDispatch()
+    const history = useHistory();
+    const user = useSelector((state) => state.session.user)
+
+    if(!user){
+        history.push(`/`)
+    }
+
+    const userId = useSelector((state)=>state.session.user.id)
+
+    async function getFavorites(){
+        let res = await fetch(`/api/sneaker/userFav/${userId}`)
+
+        let favoritesArr = res.data.userFavorites
+        dispatch(userFav(favoritesArr))
+        for(let i=0; i<favoritesArr.length; i++){
+
+            if(favoritesArr[i]['styleId'] === styleID){
+               return setFavorited(true)
+            }
+
+        }
+        return setFavorited(false)
+    }
+
+    useEffect(()=>{
+        getFavorites()
+
+    }, [dispatch])
+
+    const favorites = useSelector((state)=> state.favorites.favorites)
+
+    return (
+        <div>
+
+        </div>
+    )
+
+}
+
+export default UserFavorites;
