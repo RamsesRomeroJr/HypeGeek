1cb863a96fb2503126d88a6f05b5fa49f09bcf4d
ramses <ramses.romero.jr@gmail.com>
2021-02-13 00:56:53 -0500

    simple contact info done

--- frontend/src/components/AboutModal/aboutPage.js
+++ frontend/src/components/AboutModal/aboutPage.js
@@ -1,11 +1,20 @@
 import React from "react";
 import './aboutPage.css'
 import {FaAngellist, FaLinkedinIn, FaGithub} from 'react-icons/fa'
+import styled from 'styled-components'
+
+const Contact = styled.h1`
+    align-self:center;
+    justify-content:center;
+    font-family: 'Staatliches', cursive;
+    font-weight:1000;
+`
 
 function AboutPage (){
 
     return(
         <div className='aboutContainer'>
+            <Contact>Contact Info</Contact>
             <div className='social-links'>
                 <div className='social-btn flex-center' id='angelList'>
                     <a href='https://angel.co/u/ramses-romero-jr' target='__blank' style={{color:'#333'}}>
