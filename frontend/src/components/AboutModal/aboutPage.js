60f6278aba9d21be8941d37d6a8e442676c195dc
ramses <ramses.romero.jr@gmail.com>
2021-02-13 00:15:02 -0500

    have social links started

--- frontend/src/components/AboutModal/aboutPage.js
+++ frontend/src/components/AboutModal/aboutPage.js
@@ -1,11 +1,21 @@
-import React, {useState} from "react";
+import React from "react";
 import './aboutPage.css'
 
 function AboutPage (){
 
     return(
-        <div>
-            About
+        <div className='aboutContainer'>
+            <div className='social-links'>
+                <div className='social-btn flex-center' id='angelList'>
+                    <span>u/ramses-romero-jr</span>
+                </div>
+                <div className='social-btn flex-center' id='linkedin'>
+                    <span>in/ramses-romero-jr</span>
+                </div>
+                <div className='social-btn flex-center' id='github'>
+                    <span>RamsesRomeroJr</span>
+                </div>
+            </div>
         </div>
     )
 }
