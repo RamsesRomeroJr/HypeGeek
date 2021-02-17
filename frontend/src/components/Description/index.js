bf7c47d6212a99cffefc1b9c53c3d020ef162743
ramses <ramses.romero.jr@gmail.com>
2021-02-16 23:36:29 -0500

    all done

--- frontend/src/components/Description/index.js
+++ frontend/src/components/Description/index.js
@@ -4,14 +4,22 @@ import Description from './descriptionPage.js';
 import styled from 'styled-components';
 
 const AboutButton = styled.button`
-text-decoration:none;
-  color:grey;
-  font-family: 'Staatliches', cursive;
-  font-weight:3;
-  cursor:pointer;
+    text-decoration:none;
+    width:75px;
+    margin-bottom:4px;
+    margin-top:4px;
+    border:solid 1px lightgrey;
+    background-color:#FCFAF0;
+    color:grey;
+    border:solid 0.5px lightgrey;
+    box-shadow: 0 1px 2px 0px rgba(0,0,0,0.6);
+    outline:none;
 
-  &:hover{
+    &:hover{
     color:green;
+    border:solid 0.5px lightgrey;
+    box-shadow: 0 1px 5px 0px rgba(0,128,0,0.6);
+
   }
 `
 
