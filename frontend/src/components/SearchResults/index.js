77e217c7f6d6e5436ed46e7358bde463c01afbe7
ramses <ramses.romero.jr@gmail.com>
2021-02-22 01:51:12 -0500

    search working

--- frontend/src/components/SearchResults/index.js
+++ frontend/src/components/SearchResults/index.js
@@ -36,7 +36,7 @@ const SearchResults = () =>{
         <div>
             <ShoesContainer>
                 {!results && <h4>Loading...</h4>}
-                {! results.length && <h4>No Shoes Found</h4>}
+                {/* {! results.length && <h4>No Shoes Found</h4>} */}
                 {results && results.map(sneaker =>{
                     return <Shoe key={sneaker._id} sneaker={sneaker}/>
                 })}
